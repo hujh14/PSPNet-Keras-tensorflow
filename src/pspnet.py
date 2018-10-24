334a6df9e7c6ca45a2cfdc42c2902152b1c6f7ac
Jeffrey Hu <hujh@mit.edu>
2018-10-24 02:08:33 -0400

    run

--- src/pspnet.py
+++ src/pspnet.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python
 from __future__ import print_function
 import os
-from os.path import splitext, join, isfile, isdir, basename
+from os.path import splitext, join, isfile, isdir, basename, exists, dirname
 import argparse
+import cv2
 import numpy as np
-from scipy import misc, ndimage
+from glob import glob
+from scipy import ndimage
+
+import tensorflow as tf
 from keras import backend as K
 from keras.models import model_from_json, load_model
-import tensorflow as tf
+from keras.utils.generic_utils import CustomObjectScope
+
 import layers_builder as layers
-from glob import glob
 from python_utils import utils
 from python_utils.preprocessing import preprocess_img
-from keras.utils.generic_utils import CustomObjectScope
 
 # These are the means for the ImageNet pretrained ResNet
 DATA_MEAN = np.array([[[123.68, 116.779, 103.939]]])  # RGB order
@@ -23,20 +26,19 @@ class PSPNet(object):
 
     def __init__(self, nb_classes, resnet_layers, input_shape, weights):
         self.input_shape = input_shape
-        json_path = join("weights", "keras", weights + ".json")
-        h5_path = join("weights", "keras", weights + ".h5")
         if 'pspnet' in weights:
-            if os.path.isfile(json_path) and os.path.isfile(h5_path):
-                print("Keras model & weights found, loading...")
-                with CustomObjectScope({'Interp': layers.Interp}):
-                    with open(json_path, 'r') as file_handle:
-                        self.model = model_from_json(file_handle.read())
+            # Import using model_name
+            h5_path = join("weights", "keras", weights + ".h5")
+            json_path = join("weights", "keras", weights + ".json")
+            self.model = layers.build_pspnet(nb_classes=nb_classes,
+                                             resnet_layers=resnet_layers,
+                                             input_shape=self.input_shape)
+
+            if os.path.isfile(h5_path):
+                print("Keras weights found, loading...")
                 self.model.load_weights(h5_path)
             else:
-                print("No Keras model & weights found, import from npy weights.")
-                self.model = layers.build_pspnet(nb_classes=nb_classes,
-                                                 resnet_layers=resnet_layers,
-                                                 input_shape=self.input_shape)
+                print("No Keras weights found, import from npy weights.")
                 self.set_npy_weights(weights)
         else:
             print('Load pre-trained weights')
@@ -52,12 +54,12 @@ class PSPNet(object):
         h_ori, w_ori = img.shape[:2]
 
         # Preprocess
-        img = misc.imresize(img, self.input_shape)
+        img = cv2.resize(img, self.input_shape)
 
-        img = img - DATA_MEAN
-        img = img[:, :, ::-1]  # RGB => BGR
+        img = img - DATA_MEAN[:,:,::-1]
+        # img = img[:, :, ::-1]  # RGB => BGR
         img = img.astype('float32')
-        print("Predicting...")
+        # print("Predicting...")
 
         probs = self.feed_forward(img, flip_evaluation)
 
@@ -66,7 +68,7 @@ class PSPNet(object):
             probs = ndimage.zoom(probs, (1. * h_ori / h, 1. * w_ori / w, 1.),
                                  order=1, prefilter=False)
 
-        print("Finished prediction...")
+        # print("Finished prediction...")
 
         return probs
 
@@ -74,7 +76,7 @@ class PSPNet(object):
         assert data.shape == (self.input_shape[0], self.input_shape[1], 3)
 
         if flip_evaluation:
-            print("Predict flipped")
+            # print("Predict flipped")
             input_with_flipped = np.array(
                 [data, np.flip(data, axis=1)])
             prediction_with_flipped = self.model.predict(input_with_flipped)
@@ -84,10 +86,10 @@ class PSPNet(object):
             prediction = self.model.predict(np.expand_dims(data, 0))[0]
         return prediction
 
-    def set_npy_weights(self, weights_path):
-        npy_weights_path = join("weights", "npy", weights_path + ".npy")
-        json_path = join("weights", "keras", weights_path + ".json")
-        h5_path = join("weights", "keras", weights_path + ".h5")
+    def set_npy_weights(self, model_name):
+        npy_weights_path = join("weights", "npy", model_name + ".npy")
+        json_path = join("weights", "keras", model_name + ".json")
+        h5_path = join("weights", "keras", model_name + ".h5")
 
         print("Importing weights from %s" % npy_weights_path)
         weights = np.load(npy_weights_path, encoding='bytes').item()
@@ -139,6 +141,26 @@ class PSPNet101(PSPNet):
         PSPNet.__init__(self, nb_classes=nb_classes, resnet_layers=101,
                         input_shape=input_shape, weights=weights)
 
+def get_pspnet(model_name, weights):
+    if not args.weights:
+        if "pspnet50" in model_name:
+            pspnet = PSPNet50(nb_classes=150, input_shape=(473, 473),
+                              weights=model_name)
+        elif "pspnet101" in model_name:
+            if "cityscapes" in model_name:
+                pspnet = PSPNet101(nb_classes=19, input_shape=(713, 713),
+                                   weights=model_name)
+            if "voc2012" in model_name:
+                pspnet = PSPNet101(nb_classes=21, input_shape=(473, 473),
+                                   weights=model_name)
+
+        else:
+            print("Network architecture not implemented.")
+    else:
+        pspnet = PSPNet50(nb_classes=2, input_shape=(
+            768, 480), weights=args.weights)
+    return pspnet
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -148,14 +170,13 @@ if __name__ == "__main__":
                                  'pspnet101_cityscapes',
                                  'pspnet101_voc2012'])
     parser.add_argument('-w', '--weights', type=str, default=None)
-    parser.add_argument('-i', '--input_path', type=str, default='example_images/ade20k.jpg',
+    parser.add_argument('-i', '--input_path', type=str, default='data/example_images/ade20k.jpg',
                         help='Path the input image')
     parser.add_argument('-g', '--glob_path', type=str, default=None,
                         help='Glob path for multiple images')
-    parser.add_argument('-o', '--output_path', type=str, default='example_results/ade20k.jpg',
+    parser.add_argument('-o', '--output_dir', type=str, default='output/',
                         help='Path to output')
     parser.add_argument('--id', default="0")
-    parser.add_argument('--input_size', type=int, default=500)
     parser.add_argument('-f', '--flip', type=bool, default=True,
                         help="Whether the network should predict on both image and flipped image.")
 
@@ -163,12 +184,16 @@ if __name__ == "__main__":
 
     # Handle input and output args
     images = glob(args.glob_path) if args.glob_path else [args.input_path,]
+    images.sort()
+
+    # Directories
+    im_dir = os.path.dirname(args.input_path)
     if args.glob_path:
-        fn, ext = splitext(args.output_path)
-        if ext:
-            parser.error("output_path should be a folder for multiple file input")
-        if not isdir(args.output_path):
-            os.mkdir(args.output_path)
+        im_dir = args.glob_path.split('*')[0]
+    cm_dir = join(args.output_dir, "cm/")
+    pm_dir = join(args.output_dir, "pm/")
+    vis_dir = join(args.output_dir, "vis/")
+
 
     # Predict
     os.environ["CUDA_VISIBLE_DEVICES"] = args.id
@@ -178,45 +203,34 @@ if __name__ == "__main__":
 
     with sess.as_default():
         print(args)
-        if not args.weights:
-            if "pspnet50" in args.model:
-                pspnet = PSPNet50(nb_classes=150, input_shape=(473, 473),
-                                  weights=args.model)
-            elif "pspnet101" in args.model:
-                if "cityscapes" in args.model:
-                    pspnet = PSPNet101(nb_classes=19, input_shape=(713, 713),
-                                       weights=args.model)
-                if "voc2012" in args.model:
-                    pspnet = PSPNet101(nb_classes=21, input_shape=(473, 473),
-                                       weights=args.model)
-
-            else:
-                print("Network architecture not implemented.")
-        else:
-            pspnet = PSPNet50(nb_classes=2, input_shape=(
-                768, 480), weights=args.weights)
-
+        pspnet = get_pspnet(args.model, args.weights)
         for i, img_path in enumerate(images):
-            print("Processing image {} / {}".format(i+1,len(images)))
-            img = misc.imread(img_path, mode='RGB')
-            cimg = misc.imresize(img, (args.input_size, args.input_size))
+            print("Processing image {} / {} : {}".format(i+1,len(images), img_path))
+            img = cv2.imread(img_path)
 
             probs = pspnet.predict(img, args.flip)
 
             cm = np.argmax(probs, axis=2)
             pm = np.max(probs, axis=2)
 
-            color_cm = utils.add_color(cm)
             # color cm is [0.0-1.0] img is [0-255]
+            color_cm = utils.add_color(cm)
             alpha_blended = 0.5 * color_cm * 255 + 0.5 * img
 
-            if args.glob_path:
-                input_filename, ext = splitext(basename(img_path))
-                filename = join(args.output_path, input_filename)
-            else:
-                filename, ext = splitext(args.output_path)
+            fn = img_path.replace(im_dir, '')
+            if fn[0] == '/':
+                fn = fn[1:]
+            cm_fn = join(cm_dir, fn)
+            pm_fn = join(pm_dir, fn)
+            vis_fn = join(vis_dir, fn)
 
-            misc.imsave(filename + "_seg_read" + ext, cm)
-            misc.imsave(filename + "_seg" + ext, color_cm)
-            misc.imsave(filename + "_probs" + ext, pm)
-            misc.imsave(filename + "_seg_blended" + ext, alpha_blended)
+            if not exists(dirname(cm_fn)):
+                os.makedirs(dirname(cm_fn))
+            if not exists(dirname(pm_fn)):
+                os.makedirs(dirname(pm_fn))
+            if not exists(dirname(vis_fn)):
+                os.makedirs(dirname(vis_fn))
+
+            cv2.imwrite(cm_fn, cm)
+            cv2.imwrite(pm_fn, pm)
+            cv2.imwrite(vis_fn, alpha_blended)
